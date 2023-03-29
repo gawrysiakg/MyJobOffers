8a212743f15bcfa6e57db35c045e96012094fdf2
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-03-29 19:51:12 +0200

    added find by id endpoint and test not found id

--- src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,12 @@ public class OfferRestController {
     public ResponseEntity<List<OfferResponseDto>> getOffers(){
         List<OfferResponseDto> allOffers = offerFacade.findAllOffers();
       return ResponseEntity.ok(allOffers);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<OfferResponseDto> getOfferById(@PathVariable String id){
+        OfferResponseDto offer = offerFacade.findOfferById(id);
+        return ResponseEntity.ok(offer);
     }
 
 
