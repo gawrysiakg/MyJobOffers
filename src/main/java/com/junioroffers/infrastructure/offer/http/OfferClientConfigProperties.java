84db348806233c171e791f4f049b00d2d48f957d
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-04-04 10:44:48 +0200

    http error tests

--- src/main/java/com/junioroffers/infrastructure/offer/http/OfferClientConfigProperties.java
+++
@@ -1,9 +0,0 @@
-package com.junioroffers.infrastructure.offer.http;
-
-import lombok.Builder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@Builder
-@ConfigurationProperties(prefix = "myjoboffers.offer.http.client.config")
-public record OfferClientConfigProperties(long connectionTimeout, long readTimeout, String uri, int port) {
-}
