7154e75b6989dcff6e6f6d6deeb9f41c2765f0fb
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-08-24 09:42:32 +0200

    swagger bug

--- src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
+++ src/main/java/com/junioroffers/infrastructure/offer/controller/OfferRestController.java
@@ -4,6 +4,10 @@ import com.junioroffers.domain.offer.OfferFacade;
 import com.junioroffers.domain.offer.dto.JobOfferResponse;
 import com.junioroffers.domain.offer.dto.OfferRequestDto;
 import com.junioroffers.domain.offer.dto.OfferResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -13,12 +17,16 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+//@SecurityRequirement(name = "bearerAuth")
+//@Tag(name = "User", description = "The User API. Contains all the operations that can be performed on a user.")
+
 @RestController
 @RequestMapping("/offers")
 @RequiredArgsConstructor
 public class OfferRestController {
 
     private final OfferFacade offerFacade;
+
 
     @GetMapping
     public ResponseEntity<List<OfferResponseDto>> getOffers(){
---
+++ src/main/java/com/junioroffers/infrastructure/swagger/OpenAPISecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.junioroffers.infrastructure.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info =@Info(
+                title = "User API",
+                version = "${api.version}",
+                contact = @Contact(
+                        name = "Grzegorz Gawrysiak", email = "gawrysiak.gg@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "${tos.uri}",
+                description = "${api.description}"
+        ),
+        servers = @Server(
+                url = "${api.server.url}",
+                description = "Production"
+        )
+)
+public class OpenAPISecurityConfiguration {
+}
